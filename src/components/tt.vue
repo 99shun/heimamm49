0fdf360cddf6acb8a02b924a50bf08c6bd98fa6f
Your Name <you@example.com>
2020-04-14 20:55:52 +0800

    开始了我的PC端项目

---
+++ src/components/tt.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+export default {
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+
+</style>
