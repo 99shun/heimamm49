ce2457e11c2411bfb99103e6d295ed9bc6fa73cf
Your Name <you@example.com>
2020-04-14 21:15:38 +0800

    安装element

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入 element插件
+import ElementUI from 'element-ui';
+// 导入 element的css文件
+import 'element-ui/lib/theme-chalk/index.css';
 
+
+// 注册 element
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
