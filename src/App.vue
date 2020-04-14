ffe79a3607baecca56c1f88a45c72f92558e29b7
Your Name <you@example.com>
2020-04-14 20:59:18 +0800

    删除默认代码

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <tt msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-// import HelloWorld from '@/components/HelloWorld.vue'
-import tt from '@/components/tt.vue'
-
 export default {
-  name: 'App',
-  components: {
-    tt
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
